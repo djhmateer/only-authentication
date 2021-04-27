5a29edc94eef9744092dde7025f83e33182f945d
Dave Mateer <davemateer@gmail.com>
2021-04-27 16:25:22 +0100

    auto

--- OnlyAuthentication.Web/Startup.cs
+++ OnlyAuthentication.Web/Startup.cs
@@ -1,5 +1,7 @@
+using System.IO;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
@@ -10,16 +12,32 @@ namespace OnlyAuthentication.Web
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+
+        //public Startup(IConfiguration configuration, IHostingEnvironment environment)
+        public Startup(IConfiguration configuration, IHostEnvironment hostEnvironment)
         {
+            _hostEnvironment = hostEnvironment;
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
+        private readonly IHostEnvironment _hostEnvironment;
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // create a directory for keys if it doesn't exist
+            //it'll be created in the root, beside the wwwroot directory
+            var keysDirectoryName = "Keys";
+            //var keysDirectoryPath = Path.Combine(hostingEnvironment.ContentRootPath, keysDirectoryName);
+            var keysDirectoryPath = Path.Combine(_hostEnvironment.ContentRootPath, keysDirectoryName);
+            if (!Directory.Exists(keysDirectoryPath)) Directory.CreateDirectory(keysDirectoryPath);
+
+            services.AddDataProtection()
+                .PersistKeysToFileSystem(new DirectoryInfo(keysDirectoryPath))
+                .SetApplicationName("CustomCookieAuthentication");
+
+
             services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie();
 
             services.AddRazorPages();
